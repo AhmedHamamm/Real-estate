a45c98e0791e4a54614113d118c86107907f55fd
Ahmed Hamamm <ahamam2000@yahoo.com>
2024-06-23 19:35:56 +0300

    Add logo, popular, and value sections with updated styles and functionality
    This commit introduces the following enhancements to the real-estate website:
    
    - **Logo Section:**
      - Added a new section to display the website logo.
    
    - **Popular Section:**
      - Integrated a new slider using Swiper.js (https://swiperjs.com/) to showcase popular real estate listings.
      - Implemented JavaScript to handle the slider functionality.
      - Updated CSS styles for the popular section to ensure a visually appealing and responsive design.
    
    - **Value Section:**
      - Added a new section to highlight the key values and benefits of using the website.
      - Implemented JavaScript for a dropdown feature within the value section.
      - Updated CSS styles for the value section to maintain consistency with the overall design.
    
    These changes enhance the user experience by providing a more dynamic and interactive interface.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,46 @@ function scrollHeader() {
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+// Swiper Popular
+var swiperPopular = new Swiper(".popular-container", {
+  spaceBetween: 32,
+  grabCurspr: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Value Accordion
+const accordionItems = document.querySelectorAll(".value-accordion_item");
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value-accordion_header");
+
+  // For Click event, onClick toggle item
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    // This If condiiton  for check if item opend close other opened items.
+    toggleItem(item);
+    if (openItem && openItem !== item) toggleItem(openItem);
+  });
+});
+
+// For toggle item ( open & close).
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value-accordion_content");
+  // This If condiiton  for check if it contains open class remove it else add it.
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
