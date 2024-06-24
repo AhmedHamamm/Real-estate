073349480ec06c0da6f707f6128aa10c849cd9bc
Ahmed Hamamm <ahamam2000@yahoo.com>
2024-06-24 15:12:52 +0300

    Add contact and subscribe sections, footer, enhanced scroll bar style, dark/light theme, improved responsiveness, and scroll animations
    This commit includes several significant enhancements and additions to the real-estate website:
    
    - **Contact Section:**
        - Added a new section for users to get in touch with the website's team.
    - **Subscribe Section:**
    
    - **Footer:**
        - Added a footer to provide additional navigation links and information.
    - **Enhanced Scroll Bar Style:**
        - Improved the styling of the scroll bar for a more modern look.
    - **Dark/Light Theme:**
        - Introduced a dark/light theme switcher.
        - Implemented JavaScript functions to toggle between dark and light themes manually using a button.
        - Updated CSS to support both themes.
    - **Improved Responsiveness:**
        - Enhanced the responsiveness of the website to ensure a seamless experience across various devices and screen sizes.
    - **Scroll Animations:**
        - Integrated scroll animations using ScrollReveal (https://scrollrevealjs.org/).
        - Added JavaScript functions for the following:
            - **Scroll Section Active Link:** Highlights the active link based on the current scroll position.
            - **Show Scroll UP:** Displays a scroll-to-top button when the user scrolls down.
            - **Activate/Deactivate Theme:** Allows users to switch themes manually.
            - **ScrollReveal:** Animates elements on scroll for a more dynamic user experience.
    
    These updates significantly improve the website's functionality, usability, and visual appeal.

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,92 @@ const toggleItem = (item) => {
     item.classList.add("accordion-open");
   }
 };
+
+// Scroll Section Active Link
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav-menu a[href*="' + sectionId + '"]')
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector('.nav-menu a[href*="' + sectionId + '"]')
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+// Show Scroll UP
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the scroll-up
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+// Dark Light Theme
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// Obtain the current theme that the interface has by validating the dark-theme class.
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
+
+// Validate if the user previously chose a topic.
+if (selectedTheme) {
+  // This if condiiton for validation, I ask what the issue was to know if I activated or deactivated the Dark theme
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / Deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or Remove the dark icon theme.
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // For SAVE the theme and current icon that user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+// Scrollreveal
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true,
+});
+
+sr.reveal(
+  `.home-title, .popular-container, .subscribe-container, .footer-container`
+);
+sr.reveal(`.home-description, .footer-info`, { delay: 500 });
+sr.reveal(`.home-search`, { delay: 600 });
+sr.reveal(`.home-value`, { delay: 700 });
+sr.reveal(`.home-images`, { delay: 800, origin: "bottom" });
+sr.reveal(`.logos-img`, { interval: 100 });
+sr.reveal(`.value-images, .contact-content`, { origin: "left" });
+sr.reveal(`.value-content, .contact-images`, { origin: "right" });
